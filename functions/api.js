b1c8fe5b321c3971c34c877b552345342a4c94ae
dulanjanchanaka@gmail.com <dulanjanchanaka@gmail.com>
2024-01-07 18:45:05 +0530

    first commit

---
+++ functions/api.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const Babies = require("../routes/babies");
+const cors = require("cors");
+const serverless = require("serverless-http");
+
+dotenv.config();
+
+app.use(cors());
+app.use(express());
+app.use("/api/babies", Babies);
+
+// app.get("/", (req, res) => res.status(200).json())
+
+mongoose.connect("mongodb+srv://kobeiganehealth:kobeigane1234@cluster0.sinqmir.mongodb.net/", {useNewUrlParser: true,
+    useUnifiedTopology: true,}).then(()=>{
+        console.log("DB Connection Successfull!");
+    }).catch((err) => {
+        console.log(err)
+    })
+
+    module.exports.handler =serverless(app);
